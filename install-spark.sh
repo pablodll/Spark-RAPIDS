1b30d07cb2dba34da4ded7a823e2a85df9747573
Pablo Daurell <34186494+pablodll@users.noreply.github.com>
2022-05-30 10:30:57 +0200

    Spark instalation file

---
+++ install-spark.sh
@@ -0,0 +1,5 @@
+wget -q https://archive.apache.org/dist/spark/spark-3.2.1/spark-3.2.1-bin-hadoop3.2.tgz
+wget -q https://repo1.maven.org/maven2/com/nvidia/rapids-4-spark_2.12/22.04.0/rapids-4-spark_2.12-22.04.0.jar
+wget -q https://repo1.maven.org/maven2/ai/rapids/cudf/22.04.0/cudf-22.04.0.jar
+
+tar xf spark-3.2.1-bin-hadoop3.2.tgz
