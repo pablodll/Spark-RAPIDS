7b490d90864c7c6ae68ada59fe062ceaaf560c59
Pablo Daurell <34186494+pablodll@users.noreply.github.com>
2022-05-31 13:23:34 +0200

    Update install-spark.sh

--- install-spark.sh
+++ install-spark.sh
@@ -6,4 +6,4 @@ wget -q https://repo1.maven.org/maven2/ai/rapids/cudf/22.04.0/cudf-22.04.0.jar
 
 tar xf spark-3.2.1-bin-hadoop3.2.tgz
 
-!pip install git+https://github.com/databrickslabs/dbldatagen
+pip install git+https://github.com/databrickslabs/dbldatagen
