8acd5d0f188f35fc85632f039b56bea58c735c05
Pablo Daurell <34186494+pablodll@users.noreply.github.com>
2022-06-13 12:16:34 +0200

    Update install-spark.sh

--- install-spark.sh
+++ install-spark.sh
@@ -1,4 +1,4 @@
-apt-get install openjdk-8-jdk-headless -qq > /dev/null
+apt install openjdk-8-jdk-headless -qq > /dev/null
 
 wget -q https://archive.apache.org/dist/spark/spark-3.2.1/spark-3.2.1-bin-hadoop3.2.tgz
 wget -q https://repo1.maven.org/maven2/com/nvidia/rapids-4-spark_2.12/22.04.0/rapids-4-spark_2.12-22.04.0.jar
