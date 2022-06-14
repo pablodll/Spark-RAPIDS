fd680f9e5225fb9e4823504b636d107dfdb5ebd1
Pablo Daurell <34186494+pablodll@users.noreply.github.com>
2022-06-14 13:04:39 +0200

    Update spark-gpu.sh

--- spark-gpu.sh
+++ spark-gpu.sh
@@ -1,1 +1,1 @@
-./spark-3.2.1-bin-hadoop3.2/bin/pyspark --num-executors=1 --conf spark.plugins=com.nvidia.spark.SQLPlugin --jars '/Spark-RAPIDS/rapids-4-spark_2.12-22.04.0.jar,/Spark-RAPIDS/cudf-22.04.0.jar'
+./spark-3.2.1-bin-hadoop3.2/bin/pyspark --num-executors=1 --conf spark.plugins=com.nvidia.spark.SQLPlugin --jars '/Spark-RAPIDS/rapids-4-spark_2.12-22.04.0.jar,/Spark-RAPIDS/cudf-22.04.0-cuda11.jar'
